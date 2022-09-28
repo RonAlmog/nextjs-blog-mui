8033e064423b87d70dd72162f8d9ec98e4d2ccee
Ron Almog <ron8132@gmail.com>
2022-09-28 07:37:58 -0400

    add learn

---
+++ pages/learn/[topic]/[slug].tsx
@@ -0,0 +1,57 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+
+import hljs from "highlight.js";
+import typescript from "highlight.js/lib/languages/typescript";
+import "highlight.js/styles/vs2015.css";
+import { useEffect } from "react";
+
+hljs.registerLanguage("typescript", typescript);
+
+const components = {};
+
+export default function Learn({
+  source,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+
+  return (
+    <div style={{ width: "600px", margin: "auto" }}>
+      <MDXRemote {...source} components={components} />
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const articlesDirectory = path.join("articles");
+  const files = fs.readdirSync(articlesDirectory);
+  const paths = files.map((fileName: string) => ({
+    params: {
+      slug: fileName.replace(".mdx", ""),
+    },
+  }));
+  console.log("paths", paths);
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+type Params = {
+  [param: string]: any;
+};
+
+export const getStaticProps: GetStaticProps<Params> = async ({
+  params: { slug },
+}: Params) => {
+  const article = fs.readFileSync(path.join("articles", slug + ".mdx"));
+  const { data: metaData1, content } = matter(article);
+  const mdxSource = await serialize(content, { scope: metaData1 });
+  return { props: { source: mdxSource } };
+};

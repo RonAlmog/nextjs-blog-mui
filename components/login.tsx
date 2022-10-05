e2019831da1986e0896947d5cba767aedc631202
Ron Almog <ron8132@gmail.com>
2022-10-05 09:07:07 -0400

    build login component

--- components/login.tsx
+++ components/login.tsx
@@ -1,7 +1,17 @@
-import { Avatar, Paper } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import React from "react";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Box from "@mui/system/Box";
+import Link from "next/link";
 
 type Props = {};
 const paperStyle = {
@@ -10,15 +20,51 @@ const paperStyle = {
   width: 280,
   margin: "20px auto",
 };
+const avatarStyle = { backgroundColor: "#1bbd7e" };
 
 const Login = (props: Props) => {
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
-        <Avatar>
-          <LockOutlinedIcon />
-        </Avatar>
-        test
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Avatar style={avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Sign In</h2>
+        </Grid>
+        <TextField
+          label="Username"
+          placeholder="Enter user name"
+          fullWidth
+          required
+        />
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          fullWidth
+          type="password"
+          required
+        />
+        <FormControlLabel
+          control={<Checkbox name="" color="primary" />}
+          label="Remember Me"
+        ></FormControlLabel>
+        <Button type="submit" color="primary" variant="contained" fullWidth>
+          Sign In
+        </Button>
+
+        <Typography>
+          <Link href="#">Forgot password?</Link>
+        </Typography>
+        <Typography>
+          Have an account?
+          <Link href="#">Sign Up</Link>
+        </Typography>
       </Paper>
     </Grid>
   );

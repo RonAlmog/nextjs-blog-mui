5bfd6e3185d08fbebef70e5411360946d210ce4d
Ron Almog <ron8132@gmail.com>
2022-10-09 10:31:24 -0400

    add validation to signup page

--- components/signup.tsx
+++ components/signup.tsx
@@ -4,6 +4,7 @@ import {
   Checkbox,
   FormControl,
   FormControlLabel,
+  FormHelperText,
   FormLabel,
   Grid,
   Paper,
@@ -16,6 +17,7 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import Link from "../src/Link";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { useState } from "react";
 
 type Props = {};
 
@@ -44,7 +46,9 @@ const SignUp = (props: Props) => {
     email: Yup.string()
       .required("Email is required")
       .email("Please enter a valid email"),
-    gender: Yup.boolean().required("Please select gender"),
+    gender: Yup.string()
+      .oneOf(["male", "female"], "Please select gender")
+      .required("Please select gender"),
     phoneNumber: Yup.number().typeError("Enter a valid phone number"),
     password: Yup.string()
       .required("Password is required")
@@ -53,10 +57,12 @@ const SignUp = (props: Props) => {
     confirmPassword: Yup.string()
       .oneOf([Yup.ref("password")], "Password does not match")
       .required("Confirm password is required"),
+    termsAndConditions: Yup.bool().oneOf([true], "Accept term and condition"),
   });
   const onSubmit = (values, props) => {
     console.log(values);
   };
+
   return (
     <Grid>
       <Paper style={paperStyle}>
@@ -89,7 +95,7 @@ const SignUp = (props: Props) => {
                 variant="standard"
                 placeholder="Enter your name"
                 helperText={<ErrorMessage name="name" />}
-                error={props.errors.name}
+                error={!!props.errors.name}
               />
               <Field
                 as={TextField}
@@ -99,7 +105,7 @@ const SignUp = (props: Props) => {
                 variant="standard"
                 placeholder="Enter your email"
                 helperText={<ErrorMessage name="email" />}
-                error={props.errors.email}
+                error={!!props.errors.email}
               />
 
               <FormControl style={marginTop}>
@@ -113,7 +119,7 @@ const SignUp = (props: Props) => {
                   name="gender"
                   style={{ display: "initial" }}
                   helperText={<ErrorMessage name="gender" />}
-                  error={props.errors.gender}
+                  error={!!props.errors.gender}
                 >
                   <FormControlLabel
                     value="female"
@@ -127,6 +133,9 @@ const SignUp = (props: Props) => {
                   />
                 </Field>
               </FormControl>
+              <FormHelperText>
+                <ErrorMessage name="gender" />
+              </FormHelperText>
 
               <Field
                 as={TextField}
@@ -136,7 +145,7 @@ const SignUp = (props: Props) => {
                 variant="standard"
                 placeholder="Enter your phone number"
                 helperText={<ErrorMessage name="phoneNumber" />}
-                error={props.errors.phoneNumber}
+                error={!!props.errors.phoneNumber}
               />
               <Field
                 as={TextField}
@@ -147,7 +156,7 @@ const SignUp = (props: Props) => {
                 variant="standard"
                 placeholder="select a password"
                 helperText={<ErrorMessage name="password" />}
-                error={props.errors.password}
+                error={!!props.errors.password}
               />
               <Field
                 as={TextField}
@@ -158,17 +167,27 @@ const SignUp = (props: Props) => {
                 variant="standard"
                 placeholder="confirm your password"
                 helperText={<ErrorMessage name="confirmPassword" />}
-                error={props.errors.confirmPassword}
+                error={!!props.errors.confirmPassword}
               />
 
               <FormControlLabel
-                control={<Field as={Checkbox} name="termsAndConditions" />}
+                control={
+                  <Field
+                    as={Checkbox}
+                    name="termsAndConditions"
+                    helperText={<ErrorMessage name="termsAndConditions" />}
+                    error={!!props.errors.termsAndConditions}
+                  />
+                }
                 label="I accept terms and conditions"
               ></FormControlLabel>
-
+              <FormHelperText error={!!props.errors.termsAndConditions}>
+                <ErrorMessage name="termsAndConditions" />
+              </FormHelperText>
               <Button type="submit" variant="contained" color="primary">
                 Sign Up
               </Button>
+              {console.log(props)}
             </Form>
           )}
         </Formik>
